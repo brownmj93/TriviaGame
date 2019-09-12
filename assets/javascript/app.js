23d36608730349446c8a41ea677f7251396c58e8
brownmj93 <brownmj93@gmail.com>
2019-09-12 18:27:14 -0400

    completed homework with timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -91,59 +91,98 @@ var questions = [
             D: "1991",
         }
     }
-
-
 ];
 
+//Declare variables
 var userAnswers = [];
-var correctAnswers = [];
+var correctAnswers = ["Aconcagua in Argentina", "Hedy Lamarr", "To keep one eye adjusted to the dark for going below deck", "The Mayans", "Jupiter", "Tony the Tiger from Frosted Flakes", "Pac-Man", "A bitten hamburger beside an olive", "“The Choice of a New Generation”", "1989"];
 var answersCorrect = 0;
 var answersIncorrect = 0;
 
+var intervalId;
+var timerRunning = false;
+var time = 10;
 
+//Hide the content before pressing start button
 $("#content").hide();
 
+//When start button is clicked
 $("#startBtn").click(function () {
     event.preventDefault();
+
     $("#content").show();
     $("#startBtn").hide();
     loadQuestions();
     $(".questions").append('<button type="submit" id="doneBtn">DONE!</button>');
-
-
-    
+    runTimer();
 });
 
+//When done button is clicked
 $(document).on('click', '#doneBtn', function () {
-    event.preventDefault();
-    console.log("done button clicked");
+    checkAnswers();
 
-    for (j = 0; j < questions.length; j++) { 
-        if($('"radioBtn_' + questions.indexOf(questions[j]) +'"').is(':checked')){
-        //console.log($("input[name='radioBtn_" + questions.indexOf(questions[j]) + "']:checked").val());
-        userAnswers.push($("input[name='radioBtn_" + questions.indexOf(questions[j]) + "']:checked").val());
-        console.log(userAnswers[j]);
-        };
-    };
-    //console.log($("checked").val());
-    
-    //$("#content").empty();
-    
-    
 });
 
 function loadQuestions() {
-        //Loop through the questions & answer choices
-        for (i = 0; i < questions.length; i++) {
-            console.log(questions.indexOf(questions[i]));
-            $(".questions").append("<h4>" + questions[i].question + "</h4><br>");
-            $(".questions").append('<label><input type="radio" class="radioBtnClass" name="radioBtn_' + questions.indexOf(questions[i]) + '") value="'+ questions[i].answers.A +'" >' + questions[i].answers.A + '</label><br>');
-            $(".questions").append('<label><input type="radio" class="radioBtnClass" name="radioBtn_' + questions.indexOf(questions[i]) + '") value="'+ questions[i].answers.B +'" >' + questions[i].answers.B + '</label><br>');
-            $(".questions").append('<label><input type="radio" class="radioBtnClass" name="radioBtn_' + questions.indexOf(questions[i]) + '") value="'+ questions[i].answers.C +'" >' + questions[i].answers.C + '</label><br>');
-            $(".questions").append('<label><input type="radio" class="radioBtnClass" name="radioBtn_' + questions.indexOf(questions[i]) + '") value="'+ questions[i].answers.D +'" >' + questions[i].answers.D + '</label><br><br>');
+    //Loop through the questions & answer choices
+    for (i = 0; i < questions.length; i++) {
+        console.log(questions.indexOf(questions[i]));
+        $(".questions").append("<h4>" + questions[i].question + "</h4><br>");
+        $(".questions").append('<label><input type="radio" class="radioBtnClass" name="radioBtn_' + questions.indexOf(questions[i]) + '") value="' + questions[i].answers.A + '" >' + questions[i].answers.A + '</label><br>');
+        $(".questions").append('<label><input type="radio" class="radioBtnClass" name="radioBtn_' + questions.indexOf(questions[i]) + '") value="' + questions[i].answers.B + '" >' + questions[i].answers.B + '</label><br>');
+        $(".questions").append('<label><input type="radio" class="radioBtnClass" name="radioBtn_' + questions.indexOf(questions[i]) + '") value="' + questions[i].answers.C + '" >' + questions[i].answers.C + '</label><br>');
+        $(".questions").append('<label><input type="radio" class="radioBtnClass" name="radioBtn_' + questions.indexOf(questions[i]) + '") value="' + questions[i].answers.D + '" >' + questions[i].answers.D + '</label><br><br>');
 
-        };
-        ;
-        
-        
+    };
+    ;
+}
+
+function runTimer() {
+    if (!timerRunning) {
+        intervalId = setInterval(function () {
+            timerRunning = true;
+            time--;
+            $("#time").text("Seconds remaining: " + time);
+
+            if (time == 0) {
+                console.log("time is 0");
+                clearInterval(intervalId);
+                timerRunning = false;
+                checkAnswers();
+            }
+        }, 1000);
+
+    }
+    else {
+        console.log("timer not working");
+    }
+}
+
+function checkAnswers() {
+
+    //event.preventDefault();
+    console.log("done button clicked");
+    clearInterval(intervalId);
+    timerRunning = false;
+    console.log("stop timer");
+
+    for (j = 0; j < questions.length; j++) {
+
+        userAnswers.push($("input[name='radioBtn_" + questions.indexOf(questions[j]) + "']:checked").val());
+        console.log(userAnswers[j]);
+        //};
+        if (correctAnswers.includes(userAnswers[j])) {
+            answersCorrect++;
+            console.log("correct answers: " + answersCorrect);
+        }
+        else {
+            answersIncorrect++;
+            console.log("wrong answers: " + answersIncorrect);
+        }
+    };
+
+    $(".questions").empty();
+    $(".result").append("<h4>Final Results</h4><br>");
+    $(".result").append("<p>Correct Answers: " + answersCorrect + "</p>");
+    $(".result").append("<p>Incorrect Answers: " + answersIncorrect + "</p>");
 }
